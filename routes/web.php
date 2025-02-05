736092228e66d8d5d13fab638d1f02e1f94318f8
Devan-Winger <censinggem101@gmail.com>
2025-02-05 13:33:11 -0700

    Contacts Start

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactsController;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\TemperatureController;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,4 @@ Route::get('/', function () {
 Route::get('/hello', [HelloController::class, 'index']);
 Route::get('/temperature' , [TemperatureController::class, 'index']);
 Route::post('/temperature' , [TemperatureController::class, 'show'])->name('temperature_show');
+Route::get('/contacts', [ContactsController::class, 'index'])->name('contacts_show');
